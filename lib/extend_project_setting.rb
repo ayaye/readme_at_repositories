ee52af8e5ba4a9746145707661687539440ad8db
vit9696 <vit9696@users.noreply.github.com>
2022-04-04 01:32:23 +0300

    Try fixing settings button

--- lib/extend_project_setting.rb
+++ lib/extend_project_setting.rb
@@ -1,16 +1,25 @@
 require_dependency 'projects_helper'
 
 module ExtendProjectSetting
-  def project_settings_tabs
-    action = {
-      :name => 'readme_at_repositories', 
-      :controller => 'rar_project_settings', 
-      :action => :update, 
-      :partial => 'rar_project_settings/show', 
-      :label => :label_project_setting_title
-    }
-    tabs = super
-    tabs << action if User.current.allowed_to?(action, @project)
-    return tabs
+  def self.included(base)
+    base.class_eval do
+      prepend InstanceMethods
+      unloadable
+    end
+  end
+
+  module InstanceMethods
+    def project_settings_tabs
+      action = {
+        :name => 'readme_at_repositories', 
+        :controller => 'rar_project_settings', 
+        :action => :update, 
+        :partial => 'rar_project_settings/show', 
+        :label => :label_project_setting_title
+      }
+      tabs = super
+      tabs << action if User.current.allowed_to?(action, @project)
+      return tabs
+    end
   end
 end

a0e6967b027b9d18b5fb130d47b138fe29c036e7
vit9696 <vit9696@users.noreply.github.com>
2026-09-23 02:55:35 +0300

    Bump version

--- init.rb
+++ init.rb
@@ -4,8 +4,8 @@ Redmine::Plugin.register :readme_at_repositories do
   name 'Readme At Repositories plugin'
   author 'Tsubasa Nagasawa'
   description 'Display "README" at repository tab'
-  version '0.3.0'
-  url 'https://github.com/simeji/readme_at_repositories'
+  version '0.3.0-r1'
+  url 'https://github.com/ayaye/readme_at_repositories'
   author_url 'https://github.com/simeji/readme_at_repositories'
 
   project_module :readme_at_repository do
